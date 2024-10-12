a9d4c802b854f0ffc04751adc8aceaa2f02b2e51
ppchuchkov1 <ppchuchkov1@gmail.com>
2024-10-13 02:31:58 +0300

    Add Cancel page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StripeContainer from "./StripeForm"; // Adjust the import path if needed
 import SuccessPage from "./SuccessPage"; // Adjust the import path if needed
-
+import CancelPage from "./CancelPage";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StripeContainer />} />
         <Route path="/success" element={<SuccessPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
       </Routes>
     </Router>
   );
