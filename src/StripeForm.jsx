9ccd8e87695a627e4001c5f3835567cc34871e40
ppchuchkov1 <ppchuchkov1@gmail.com>
2024-10-13 02:15:00 +0300

    add success page and routing

---
+++ src/StripeForm.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+// Your public Stripe key
+const stripePromise = loadStripe(
+  "pk_test_51Hkv2kGETpcP6ndNqcDK55NUzHUgiLIDAcOdEyMNyyYTMBKsmo0YsRja7LZDuQcQj2PdOe3dqglbSkQR7Yq1FIBV00xgkelsaE"
+);
+
+const CheckoutForm = () => {
+  const [email, setEmail] = useState(""); // State for email input
+  const [products, setProducts] = useState([
+    {
+      name: "Product 1",
+      amount: 1000, // Price in cents
+      currency: "bgn",
+      quantity: 1,
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/3/34/EIZO_Foris_FG2421_VGA_computer_monitor_displaying_test_pattern.png",
+    },
+    {
+      name: "Product 2",
+      amount: 2000,
+      currency: "bgn",
+      quantity: 1,
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/3/34/EIZO_Foris_FG2421_VGA_computer_monitor_displaying_test_pattern.png",
+    },
+  ]);
+
+  // Function to handle payment
+  const handleCheckout = async () => {
+    try {
+      const lineItems = products.map((product) => ({
+        price_data: {
+          currency: product.currency,
+          product_data: {
+            name: product.name,
+            images: [product.imageUrl], // Update this line to use images instead of metadata
+          },
+          unit_amount: product.amount,
+        },
+        quantity: product.quantity,
+      }));
+
+      const response = await fetch(
+        "https://test-express-docker-1.onrender.com/api/payment/create-checkout-session",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ line_items: lineItems, customerEmail: email }),
+        }
+      );
+      const { id: sessionId } = await response.json();
+
+      const stripe = await stripePromise;
+      const { error } = await stripe.redirectToCheckout({ sessionId });
+
+      if (error) {
+        console.error("Stripe error:", error);
+      }
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Checkout</h2>
+      <input
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)} // Update email state
+        required // Optional: Add required attribute for form validation
+      />
+      <ul>
+        {products.map((product, index) => (
+          <li key={index} style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={product.imageUrl}
+              alt={product.name}
+              style={{ width: "100px", height: "100px", marginRight: "10px" }}
+            />
+            <div>
+              <div>
+                {product.name} - ${(product.amount / 100).toFixed(2)} x{" "}
+                {product.quantity}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleCheckout}>Proceed to Checkout</button>
+    </div>
+  );
+};
+
+const StripeContainer = () => (
+  <Elements stripe={stripePromise}>
+    <CheckoutForm />
+  </Elements>
+);
+
+export default StripeContainer;
